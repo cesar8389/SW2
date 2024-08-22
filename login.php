791125b696c5d1cbd056deea5855bc9d0900df13
César Augusto <cesar.ab@icloud.com>
2024-08-22 11:14:07 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,70 @@
+<?php
+include('conexao.php');
+
+if (isset($_POST['usuario']) || isset($_POST['senha'])) {
+
+    if (strlen($_POST['usuario']) == 0) {
+        echo "Preencha seu usuario";
+    } else if (strlen($_POST['senha']) == 0) {
+        echo "Preencha sua senha";
+    } else {
+
+        $usuario = $mysqli->real_escape_string($_POST['usuario']);
+        $senha = $mysqli->real_escape_string($_POST['senha']);
+
+        $sql_code = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha'";
+        $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
+
+        $quantidade = $sql_query->num_rows;
+
+        if ($quantidade == 1) {
+
+            $usuario = $sql_query->fetch_assoc();
+
+            if (!isset($_SESSION)) {
+                session_start();
+            }
+
+            $_SESSION['id'] = $usuario['id'];
+            $_SESSION['nome'] = $usuario['nome'];
+
+            header("Location: painel.php");
+
+        } else {
+            echo "Falha ao logar! E-mail ou senha incorretos";
+        }
+
+    }
+
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <body>
+        <h1>Acesse sua conta</h1>
+        <form action="" method="POST">
+            <p>
+                <label>Usuario</label>
+                <input type="text" name="usuario">
+            </p>
+            <p>
+                <label>Senha</label>
+                <input type="password" name="senha">
+            </p>
+            <p>
+                <button type="submit">Entrar</button>
+            </p>
+        </form>
+    </body>
+</body>
+
+</html>
